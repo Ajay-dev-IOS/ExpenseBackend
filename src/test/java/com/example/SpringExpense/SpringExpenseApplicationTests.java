f66a2644e1d975f6316213f2bfaa67e3d81275c1
Ajay-dev-IOS <ajayteja1299@gmail.com>
2025-09-08 16:05:35 +0530

    Initial commit

---
+++ src/test/java/com/example/SpringExpense/SpringExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringExpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
